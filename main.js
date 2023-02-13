eb4bfc2a4b646047714753c6f61914680b1349ea
Kyle Knutson <knutson.kyle86@gmail.com>
2023-02-13 10:31:12 -0800

    Add black canvas and ball

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 var canvas; 
 var canvasContext;
+var ballX = 255;
+var ballY = 255;
+var ballSize = 5;
+var ballSpeed;
 
 
 window.onload = function() {
-    console.log("Hello World!")
     canvas = document.getElementById('gameCanvas');
-    console.log(canvas);
-    canvasContext =canvas.getContext();
+    canvasContext =canvas.getContext('2d');
+    
+    var framesPerSecond = 30;
+    setInterval(drawEverything, 1000/framesPerSecond )
+}
+
+function drawBall() {
+    canvasContext.fillStyle = 'white';
+    canvasContext.beginPath();
+    canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true)
+    canvasContext.fill();
+}
+
+function colorRect(leftX,topY, width,height, drawColor){
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX,topY, width,height);
+}
+
+function drawEverything() {
+    colorRect(0,0, canvas.width,canvas.height, 'black');
+    drawBall();
 }
