9e983493f047a8cc18a7521384a805d19ba3c4b5
Kyle Knutson <knutson.kyle86@gmail.com>
2023-02-14 06:14:53 -0800

    Add ball movement

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var canvasContext;
 var ballX = 255;
 var ballY = 255;
 var ballSize = 5;
-var ballSpeed;
+var ballSpeedX = 6;
+var ballSpeedY = 4;
 
 
 window.onload = function() {
@@ -11,13 +12,16 @@ window.onload = function() {
     canvasContext =canvas.getContext('2d');
     
     var framesPerSecond = 30;
-    setInterval(drawEverything, 1000/framesPerSecond )
+    setInterval(function() {
+            moveEverything();
+            drawEverything();
+    }, 1000/framesPerSecond )
 }
 
-function drawBall() {
-    canvasContext.fillStyle = 'white';
+function drawBall(centerX,centerY, radius,color) {
+    canvasContext.fillStyle = color;
     canvasContext.beginPath();
-    canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true)
+    canvasContext.arc(centerX,centerY, radius, 0, Math.PI*2, true)
     canvasContext.fill();
 }
 
@@ -26,7 +30,17 @@ function colorRect(leftX,topY, width,height, drawColor){
     canvasContext.fillRect(leftX,topY, width,height);
 }
 
+function moveBall(){
+    ballX += ballSpeedX;
+}
+
+// BIG MOVERS #######
+function moveEverything() {
+    moveBall();
+}
+
 function drawEverything() {
     colorRect(0,0, canvas.width,canvas.height, 'black');
-    drawBall();
+    drawBall(ballX,ballY, 10,'white');
 }
+
